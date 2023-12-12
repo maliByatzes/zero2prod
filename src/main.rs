7994ab6fb845e147de9fd46336f90930bcfeab1e
maliByatzes <malib2027@gmail.com>
2023-12-12 17:51:06 +0200

    init form endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
-    let port = listener.local_addr().unwrap().port();
-    println!("Server started http://127.0.0.1:{}", port);
-    run(listener)?.await
+    let address = TcpListener::bind("127.0.0.1:0")?;
+    run(address)?.await
 }
