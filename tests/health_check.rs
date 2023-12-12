d47e4ed3b2485af84fb0da9778caad38cd3ded12
maliByatzes <malib2027@gmail.com>
2023-12-13 00:26:13 +0200

    update CI testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -47,7 +47,7 @@ async fn subscribe_returns_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     // Assert
-   assert_eq!(200, response.status().as_u16());
+    assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
         .fetch_one(&mut connection)
