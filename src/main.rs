17748b0a7f58ed69bdad395cc99c247f8403f389
maliByatzes <malib2027@gmail.com>
2023-12-12 13:35:34 +0200

    add random port for address

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Server started http://127.0.0.1:{}", port);
+    run(listener)?.await
 }
